3ec8828248a3f18fc350c62d0b2cc23f74054ca5
Will Burke <billyburke1012@gmail.com>
2021-01-25 14:05:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  const[red, setRed] = useState(23)
+  const[green, setGreen] = useState(10)
+  const[blue, setBlue] = useState(9)
+
+  const appStyle = {
+    backgroundColor: `rgb(${red}, ${green}, ${blue})`
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fakey-body" style={appStyle}> 
+      R: <input type="range" min="0" max="255"
+
+      value={red}
+      onChange={(event) => {
+        //console.log(event.target.value);
+        setRed(event.target.value)
+      }}
+      
+      />
+      {red}
+      <br></br>
+
+
+      G: <input type="range" min="0" max="255"
+
+        value={green}
+        onChange={(event) => {
+          //console.log(event.target.value);
+          setGreen(event.target.value)
+        }}
+
+        />
+        {green}
+
+        <br></br>
+
+      B: <input type="range" min="0" max="255"
+
+          value={blue}
+          onChange={(event) => {
+            //console.log(event.target.value);
+            setBlue(event.target.value)
+          }}
+
+          />
+          {blue}
+    
     </div>
   );
 }
